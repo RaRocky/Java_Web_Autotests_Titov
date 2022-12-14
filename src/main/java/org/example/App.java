2f96f3a239423de68a2aa7495acf37cb4249e5c2
Dmitry_Titov <cntitov@gmail.com>
2022-12-14 19:50:35 +0300

    Создание проекта Maven. Внесение изменений в POM.xml. Внесение изменений в классы App  и AppTest с целью создания демо-теста.

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,17 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        demo();
+    }
+
+    public static void demo () {
+        System.out.println( "Demo Test." );
+    }
+}

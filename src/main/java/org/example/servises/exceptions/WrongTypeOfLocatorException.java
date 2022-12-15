90ad97a971a00ee509b914c3e1fbc82af4c6fe9c
Dmitry_Titov <cntitov@gmail.com>
2022-12-15 05:04:23 +0300

    Создан Exception WrongTypeOfLocatorException.

---
+++ src/main/java/org/example/servises/exceptions/WrongTypeOfLocatorException.java
@@ -0,0 +1,7 @@
+package org.example.servises.exceptions;
+
+public class WrongTypeOfLocatorException extends Exception {
+    public WrongTypeOfLocatorException (String message) {
+        super(message);
+    }
+}

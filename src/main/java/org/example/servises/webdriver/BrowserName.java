b93fca20c31f0359064bac27df610a38061bb536
Dmitry_Titov <cntitov@gmail.com>
2022-12-15 04:59:38 +0300

    Создан Enum BrowserName.

---
+++ src/main/java/org/example/servises/webdriver/BrowserName.java
@@ -0,0 +1,7 @@
+package org.example.servises.webdriver;
+
+// Список браузеров, используемых для тестирования.
+public enum BrowserName {
+    CHROME, // Google Chrome.
+    FIREFOX; // Mozilla Firefox.
+}

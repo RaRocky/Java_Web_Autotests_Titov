819185d0c26e2e6adff813896d6804322bcb1a2a
Dmitry_Titov <cntitov@gmail.com>
2023-01-17 16:00:32 +0300

    Home Work №7. DONE!

--- src/test/java/org/example/abstractions/AbstractTest.java
+++ src/test/java/org/example/abstractions/AbstractTest.java
@@ -5,6 +5,10 @@ import org.example.servises.webdriver.BrowserName;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.logging.LogEntry;
+import org.openqa.selenium.logging.LogType;
+
+import java.util.List;
 
 public class AbstractTest {
 
@@ -18,6 +22,22 @@ public class AbstractTest {
     @AfterEach
     public void deleteCookie() {
         browser.getWebDriver().manage().deleteAllCookies();
+    }
+
+    @AfterEach
+    public void checkBrowser(){
+        List<LogEntry> allLogRows = getBrowser().getWebDriver().manage().logs().get(LogType.BROWSER).getAll();
+        if(!allLogRows.isEmpty()){
+
+            if (allLogRows.size() > 0 ) {
+                allLogRows.forEach(logEntry -> {
+                    System.out.println(logEntry.getMessage());
+                });
+
+            }
+
+        }
+
     }
 
     @AfterAll
